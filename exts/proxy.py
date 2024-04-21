a30e76bfb547f685fe2085f624b13ba166962493
448997701 <448997701@qq.com>
2024-04-21 22:47:19 +0800

    优化开机自己启动中转和恢复路由与标记规则

--- exts/proxy.py
+++ exts/proxy.py
@@ -18,7 +18,6 @@ import socket
 
 # Configure logging
 logging.basicConfig(filename='/var/log/xos.txt', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
 # xray配置文件路径
 CONFIG_PATH = '/usr/local/xos/xray/config.json'
 CHECK_PATH = '/usr/local/xos/xray/xray-check.json'
