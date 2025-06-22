f492252f06f7ad7692b096493a9bf31b2db960ff
448997701 <448997701@qq.com>
2025-06-22 22:44:17 +0800

    xos使用服务模式运行

---
+++ xray/xos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#停止之前的app
+echo "Stopping previous app..."
+screen -S XOS -X quit  # 假设你的screen会话名字为app_session
+#激活虚拟环境
+echo "Activating virtual environment..."
+source /usr/local/flask/bin/activate
+#启动新的app
+echo "Starting the app..."
+screen -dmS XOS python3 /usr/local/xos/app.py
+echo "App started successfully."
